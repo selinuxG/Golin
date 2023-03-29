a72fe493a3dad92904d702d501b36f9fb20e61cc
gys <selinxg@163.com>
2023-03-29 08:47:18 +0800

    修改zap参数，控制台与写入文件分开

--- config/zap.go
+++ config/zap.go
@@ -13,46 +13,42 @@ var (
 )
 
 func init() {
-	hook := lumberjack.Logger{
-		Filename:   global.SuccessLog, // 日志文件路径
-		MaxSize:    128,               // 每个日志文件保存的最大尺寸 单位：M
-		MaxBackups: 30,                // 日志文件最多保存多少个备份
-		MaxAge:     7,                 // 文件最多保存多少天
-		Compress:   true,              // 是否压缩
-	}
-
-	encoderConfig := zapcore.EncoderConfig{
+	consoleEncoder := zapcore.NewConsoleEncoder(zapcore.EncoderConfig{
 		TimeKey:        "time",
 		LevelKey:       "level",
 		NameKey:        "logger",
-		CallerKey:      "信号",
-		MessageKey:     "提示",
-		StacktraceKey:  "stacktrace",
+		MessageKey:     "msg",
+		EncodeLevel:    zapcore.CapitalColorLevelEncoder,
+		EncodeTime:     zapcore.TimeEncoderOfLayout("2006-01-02 15:04:05"),
 		LineEnding:     zapcore.DefaultLineEnding,
-		EncodeLevel:    zapcore.LowercaseLevelEncoder,  // 小写编码器
-		EncodeTime:     zapcore.ISO8601TimeEncoder,     // ISO8601 UTC 时间格式
-		EncodeDuration: zapcore.SecondsDurationEncoder, //
-		EncodeCaller:   zapcore.FullCallerEncoder,      // 全路径编码器
-		EncodeName:     zapcore.FullNameEncoder,
-	}
+		EncodeDuration: zapcore.SecondsDurationEncoder,
+	})
 
-	// 设置日志级别
-	atomicLevel := zap.NewAtomicLevel()
-	atomicLevel.SetLevel(zap.InfoLevel)
+	// 创建控制台输出的核心对象，设置级别为 info
+	consoleDebugging := zapcore.Lock(os.Stdout)
+	consoleCore := zapcore.NewCore(consoleEncoder, consoleDebugging, zap.InfoLevel)
 
-	core := zapcore.NewCore(
-		zapcore.NewJSONEncoder(encoderConfig),                                           // 编码器配置
-		zapcore.NewMultiWriteSyncer(zapcore.AddSync(os.Stdout), zapcore.AddSync(&hook)), // 打印到控制台和文件
-		atomicLevel, // 日志级别
-	)
-
-	// 开启开发模式，堆栈跟踪
-	caller := zap.AddCaller()
-	// 开启文件及行号
-	development := zap.Development()
-	// 设置初始化字段
-	//filed := zap.Fields(zap.String("serviceName", "serviceName"))
-	// 构造日志
-	Log = zap.New(core, caller, development)
+	// 创建文件输出的核心对象，设置级别为 warn
+	fileEncoder := zapcore.NewJSONEncoder(zapcore.EncoderConfig{
+		TimeKey:        "time",
+		LevelKey:       "level",
+		NameKey:        "logger",
+		MessageKey:     "msg",
+		EncodeLevel:    zapcore.CapitalLevelEncoder,
+		EncodeTime:     zapcore.TimeEncoderOfLayout("2006-01-02 15:04:05"),
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeDuration: zapcore.SecondsDurationEncoder,
+	})
+	fileDebugging := zapcore.Lock(zapcore.AddSync(&lumberjack.Logger{
+		Filename:   global.SuccessLog,
+		MaxSize:    100, // MB
+		MaxBackups: 3,
+		MaxAge:     30, // days
+	}))
+	fileCore := zapcore.NewCore(fileEncoder, fileDebugging, zap.WarnLevel)
+	// 合并两个核心对象
+	core := zapcore.NewTee(consoleCore, fileCore)
+	// 创建logger
+	Log = zap.New(core)
 
 }
