afdb96e0fd9ea935ae2a9acbe3eca75ba1314fc4
gys <selinxg@163.com>
2023-05-04 15:32:59 +0800

    增加耗时

--- main.go
+++ main.go
@@ -4,9 +4,14 @@ Copyright © 2022 高业尚
 package main
 
 import (
+	"fmt"
 	"golin/cmd"
+	"time"
 )
 
 func main() {
+	start := time.Now()
 	cmd.Execute()
+	end := time.Now().Sub(start)
+	fmt.Printf("\n[*] 任务结束,耗时: %s\n", end)
 }
