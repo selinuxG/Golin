8f9f1170d108fa9f83d61c771b89133b41a2d1c6
gys <selinxg@163.com>
2023-05-05 13:39:27 +0800

    设置编码为utf-8;增加依赖包文件

--- python/Gui.py
+++ python/Gui.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import tkinter as tk
 import tkinter.messagebox
 import os
--- python/hw.py
+++ python/hw.py
@@ -4,14 +4,17 @@ import time
 import sys
 import os
 
+
 class ssh_con():
-    def __init__(self,host,port,username,password):
+    def __init__(self, host, port, username, password):
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(hostname=host, port=port, username=username, password=password,allow_agent=False,look_for_keys=False,timeout=10)
+        ssh.connect(hostname=host, port=port, username=username, password=password, allow_agent=False,
+                    look_for_keys=False, timeout=10)
         self.command = ssh.invoke_shell()
         self.date = ""
-    def cmd_date(self,cmd):
+
+    def cmd_date(self, cmd):
         for i in cmd:
             self.command.send(i)
             self.command.send("\n                      \n")
@@ -20,10 +23,12 @@ class ssh_con():
             self.date += self.output
         return self.date
 
+
 file_name = sys.argv[2] + "_" + sys.argv[3] + ".log"
 
-file_date = ssh_con(host=sys.argv[3],port=sys.argv[6],username=sys.argv[4],password=sys.argv[5]).cmd_date(sys.argv[7].split(";"))
+file_date = ssh_con(host=sys.argv[3], port=sys.argv[6], username=sys.argv[4], password=sys.argv[5]).cmd_date(
+    sys.argv[7].split(";"))
 
-with open(os.path.join(sys.argv[1],file_name),"w+",encoding="utf-8")  as f:
+with open(os.path.join(sys.argv[1], file_name), "w+", encoding="utf-8") as f:
     for i in file_date:
-        f.write(i.replace("\n",""))
+        f.write(i.replace("\n", ""))
