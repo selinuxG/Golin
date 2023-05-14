987dc7af1f480fa27bff26938cbefffbe8e54372
gys <selinuxg@163.com>
2023-05-14 22:47:46 +0800

    MySQL连接超时调整为1.5s；增加执行命令以及调整输出结果

--- run/Mysql.go
+++ run/Mysql.go
@@ -17,6 +17,9 @@ import (
 	"unsafe"
 )
 
+// MySQL默认用户，来源官网：https://dev.mysql.com/doc/refman/8.0/en/reserved-accounts.html
+var defaultuser = []string{"root", "mysql.session", "mysql.sys", "mysql.infoschema"}
+
 type VariablGlobal struct {
 	Key   string `gorm:"column:Variable_name"`
 	Value string `gorm:"column:Value"`
@@ -79,7 +82,7 @@ func Mysql(cmd *cobra.Command, args []string) {
 // RunMysql 执行sql语句
 func RunMysql(myname string, myuser string, mypasswd string, myhost string, myport string) {
 	defer wg.Done()
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", myuser, mypasswd, myhost, myport, "mysql")
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local&timeout=1.5s", myuser, mypasswd, myhost, myport, "mysql")
 	db, err := gorm.Open("mysql", dsn)
 	defer db.Close()
 	if err != nil {
@@ -125,11 +128,34 @@ func RunMysql(myname string, myuser string, mypasswd string, myhost string, mypo
 		write.Flush()
 		return
 	}
-	//执行预制sql命令
+
+	//版本信息
+	write.WriteString("版本信息:")
+	rows, _ := db.Raw("select version()").Rows()
+	var version string
+	for rows.Next() {
+		rows.Scan(&version)
+	}
+	write.WriteString(version + "  ")
+	//此次连接ID可与查询日志关联
+	write.WriteString("本次连接ID:")
+	rows, _ = db.Raw("select connection_id()").Rows()
+	var CONNECTION_ID string
+	for rows.Next() {
+		rows.Scan(&CONNECTION_ID)
+	}
+	write.WriteString(CONNECTION_ID + "  (MySQL服务器会为每个客户端连接分配一个唯一的连接ID，在执行SQL查询、更新、删除等操作时，可以使用该连接ID来标识当前的连接。如果开启general_log后可基于次ID查询记录。)\n\n")
+	//用户信息
+	write.WriteString("------------------------------查询用户相关：身份鉴别、访问控制基本都基于以下内容\n")
 	var userlist []Userslist //用户列表信息
 	db.Raw(`select user,host,authentication_string,plugin,ssl_type,account_locked,password_lifetime,password_expired,password_last_changed from mysql.user`).Scan(&userlist)
 	for _, v := range userlist {
-		write.WriteString("------------------------------用户: " + v.User + "\n")
+		//是否为默认账户
+		if checkdefultuser(v.User) {
+			write.WriteString("------------------------------用户: " + v.User + "(此账号为默认账户)\n")
+		} else {
+			write.WriteString("------------------------------用户: " + v.User + "\n")
+		}
 		write.WriteString("连接方式：" + v.Host + "\n")
 		write.WriteString("密码信息：" + v.AuthenticationString + "\n")
 		write.WriteString("密码加密插件：" + v.Plugin + "	(mysql_native_password采用SHA1+盐值机制；caching_sha2_password采用SHA256+加盐机制)\n")
@@ -148,9 +174,9 @@ func RunMysql(myname string, myuser string, mypasswd string, myhost string, mypo
 		}
 		write.WriteString("权限相关：" + grantdata + "\n")
 		if strings.Contains(grantdata, "GRANT ALL PRIVILEGES ON *.*") || strings.Contains(grantdata, "GRANT SUPER") {
-			write.WriteString("是否为超级管理员(最好自己基于上述权限自己判断,仅供参考)：" + "	是\n")
+			write.WriteString("账户类别：超级管理员(人工基于上述权限自己判断,仅供参考)\n")
 		} else {
-			write.WriteString("是否为超级管理员(最好自己基于上述权限自己判断,仅供参考)：" + "	否\n")
+			write.WriteString("账户类别：(人工基于上述权限自己判断,仅供参考)\n")
 		}
 		write.WriteString("\n\n")
 	}
@@ -169,10 +195,29 @@ func RunMysql(myname string, myuser string, mypasswd string, myhost string, mypo
 	}
 
 	//全局密码过期时间
-	write.WriteString("\n------------------------------新建用户密码过期策略（MySQL5.7测试不生效，必须手动配置，所以结果以单个用户的过期时间为准）\n")
+	write.WriteString("\n------------------------------密码过期时间：此变量定义全局自动密码过期策略，默认值为 0，即禁用自动密码过期。如果的值为正整数N，则表示允许的密码生存期，必须M天更改密码。密码期限是从其最近一次密码更改的日期和时间开始评估的。）\n")
 	db.Raw(`show variables like 'default_password_lifetime'`).Scan(&variables)
 	if len(variables) == 1 {
 		write.WriteString(variables[0].Key + ": " + variables[0].Value + "	(过期天数)\n")
+	}
+	//查询失败锁定次数
+	write.WriteString("\n------------------------------失败锁定策略:\n")
+	db.Raw(`show variables like '%connection_control%'`).Scan(&variables)
+	if len(variables) == 3 {
+		write.WriteString(variables[0].Key + ": " + variables[0].Value + "	(在服务器为后续连接尝试添加延迟之前允许帐户连续失败的连接尝试次数)\n")
+		write.WriteString(variables[1].Key + ": " + variables[1].Value + "	(超过阈值的连接失败的最大延迟,以毫秒为单位)\n")
+		write.WriteString(variables[1].Key + ": " + variables[1].Value + "	(超过阈值的连接失败的最小延迟,以毫秒为单位)\n")
+	}
+
+	//查询超时功能，不生效，默认不符合
+	write.WriteString("\n------------------------------超时策略(即使在存在超时情况下会提示重新连接然后正常执行命令返回结果，无法达到用户退出。):\n")
+	db.Raw(`show global variables like 'connect_timeout'`).Scan(&variables)
+	if len(variables) == 1 {
+		write.WriteString(variables[0].Key + ": " + variables[0].Value + "	(登录时连接超时，单位为秒，配置后不生效，联盟也提出此问题。默认不符合)\n")
+	}
+	db.Raw(`show global variables like 'wait_timeout'`).Scan(&variables)
+	if len(variables) == 1 {
+		write.WriteString(variables[0].Key + ": " + variables[0].Value + "	(登录后连接超时，单位为秒，配置后不生效，联盟也提出此问题。默认不符合)\n")
 	}
 
 	//日志相关
@@ -192,24 +237,6 @@ func RunMysql(myname string, myuser string, mypasswd string, myhost string, mypo
 	if len(variables) == 1 {
 		write.WriteString(variables[0].Key + ": " + variables[0].Value + "	(查询日志存放方式：FILE为文件（默认）TABLE为数据表)\n")
 	}
-
-	//版本信息
-	write.WriteString("\n查询版本:	")
-	rows, _ := db.Raw("select version()").Rows()
-	var version string
-	for rows.Next() {
-		rows.Scan(&version)
-	}
-	write.WriteString(version + "\n")
-	//此次连接ID可与查询日志关联
-	write.WriteString("\n连接ID:	")
-	rows, _ = db.Raw("select connection_id()").Rows()
-	var CONNECTION_ID string
-	for rows.Next() {
-		rows.Scan(&CONNECTION_ID)
-	}
-	write.WriteString(CONNECTION_ID + "  (MySQL服务器会为每个客户端连接分配一个唯一的连接ID，在执行SQL查询、更新、删除等操作时，可以使用该连接ID来标识当前的连接。)\n")
-	//SELECT CONNECTION_ID();
 
 	//最后写入文件
 	err = write.Flush()
@@ -262,5 +289,16 @@ func echosqlfie(stratic bool, path string) {
 		}
 		fmt.Println(string(content))
 	}
+
+}
+
+// checkdefultuser 严重是否为MySQL默认账户
+func checkdefultuser(user string) bool {
+	for _, s := range defaultuser {
+		if user == s {
+			return true
+		}
+	}
+	return false
 
 }
