60c7ea2bc121f73b2d2dfb84344610c314b0cafb
SelinuxG <34471076+selinuxG@users.noreply.github.com>
2023-05-16 21:25:20 +0800

    web模式设置为https

---
+++ web/cert.go
@@ -0,0 +1,63 @@
+package web
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"math/big"
+	"os"
+	"time"
+)
+
+func CreateCert() {
+	priv, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		panic(err)
+	}
+
+	notBefore := time.Now()
+	notAfter := notBefore.Add(365 * 24 * time.Hour)
+
+	template := x509.Certificate{
+		// 设置证书版本为 v3
+		Version:      3,
+		SerialNumber: big.NewInt(1),
+		Subject: pkix.Name{
+			Organization: []string{"高业尚"},
+			CommonName:   "Golin安全加密",
+		},
+		Issuer: pkix.Name{
+			CommonName: "高业尚",
+		},
+		NotBefore:             notBefore,
+		NotAfter:              notAfter,
+		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		BasicConstraintsValid: true,
+		IsCA:                  true,
+	}
+
+	derBytes, err := x509.CreateCertificate(rand.Reader, &template, &template, &priv.PublicKey, priv)
+	if err != nil {
+		panic(err)
+	}
+
+	certOut, err := os.Create("cert.pem")
+	if err != nil {
+		panic(err)
+	}
+	defer certOut.Close()
+
+	pem.Encode(certOut, &pem.Block{Type: "CERTIFICATE", Bytes: derBytes})
+
+	keyOut, err := os.Create("key.pem")
+	if err != nil {
+		panic(err)
+	}
+	defer keyOut.Close()
+
+	pkcs1PrivateKey := x509.MarshalPKCS1PrivateKey(priv)
+	pem.Encode(keyOut, &pem.Block{Type: "RSA PRIVATE KEY", Bytes: pkcs1PrivateKey})
+}
