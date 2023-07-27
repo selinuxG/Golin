ec794d9650474d69d47033e8c163605ea6c461b4
gys <selinuxg@163.com>
2023-07-27 17:22:56 +0800

    更新web目录内置扫描字典

--- global/version.go
+++ global/version.go
@@ -1,7 +1,7 @@
 package global
 
 const (
-	Version      = "v3.2"                                                            //当前版本号
-	Releasenotes = "优化多个功能,目录扫描增加内置字典、移除pyton代码等"                                    //版本说明
+	Version      = "v3.2.1"                                                          //当前版本号
+	Releasenotes = "更新web目录内置扫描字典"                                                   //版本说明
 	RepoUrl      = "https://api.github.com/repos/selinuxg/Golin-cli/releases/latest" //仓库最新版本
 )
