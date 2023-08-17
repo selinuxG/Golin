de8fe862287df3e91de47052c3a864809c61567c
gys <selinuxg@163.com>
2023-08-17 17:30:40 +0800

    修复Linux下编译问题

--- port/crack/rdp_linux.go
+++ port/crack/rdp_linux.go
@@ -7,15 +7,5 @@ import (
 )
 
 // linux系统是不支持rdp弱口令扫描
-func rdpcon(ctx context.Context, cancel context.CancelFunc, ip, user, passwd string, port, timeout int) {
-	defer func() {
-		wg.Done()
-		<-ch
-	}()
-	select {
-	case <-ctx.Done():
-		return
-	default:
-		cancel()
-	}
+func rdpcon(cancel context.CancelFunc, ip, user, passwd string, port, timeout int) {
 }
