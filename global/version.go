7a7330fef275d1ed7072b8657aa2650a04903b48
gys <selinuxg@163.com>
2023-08-23 17:19:43 +0800

    增加识别ThinkPHP5.x版本任意命令执行、ThinkPHP3.x版本日志信息泄露、用友nc-BshServlet接口执行任意命令、用友nc目录遍历漏洞

--- global/version.go
+++ global/version.go
@@ -1,7 +1,7 @@
 package global
 
 const (
-	Version      = "1.2.2"                                                       //当前版本号
-	Releasenotes = "port模式下不指定特定地址则自动判断当前网络进行扫描;弱口令以及漏洞信息补充保存到xlsx中"             //版本说明
-	RepoUrl      = "https://api.github.com/repos/selinuxg/Golin/releases/latest" //仓库最新版本
+	Version      = "1.2.2"                                                                          //当前版本号
+	Releasenotes = "增加识别ThinkPHP5.x版本任意命令执行、ThinkPHP3.x版本日志信息泄露、用友nc-BshServlet接口执行任意命令、用友nc目录遍历漏洞" //版本说明
+	RepoUrl      = "https://api.github.com/repos/selinuxg/Golin/releases/latest"                    //仓库最新版本
 )
