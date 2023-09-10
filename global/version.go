d3237eb671e9b5a38b9ff59c7cb6ba8973a62bd9
SelinuxG <34471076+selinuxG@users.noreply.github.com>
2023-09-10 22:39:51 +0800

    增加识别CVE-2023-4415

--- global/version.go
+++ global/version.go
@@ -2,6 +2,6 @@ package global
 
 const (
 	Version      = "1.2.3"                                                       //当前版本号
-	Releasenotes = "增加识别亿邮电子邮件系统CNVD-2021-26422任意命令执行漏洞"     //版本说明
+	Releasenotes = "增加多种漏洞扫描"                                                    //版本说明
 	RepoUrl      = "https://api.github.com/repos/selinuxg/Golin/releases/latest" //仓库最新版本
 )
