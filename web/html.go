b14162a0ed5d8fd9b20293cecb485fe0f859ebe7
1127056420 <1127056420@qq.com>
2023-10-17 13:27:50 +0000

    style: 调整tpl为template

--- web/html.go
+++ web/html.go
@@ -2,46 +2,46 @@ package web
 
 import "embed"
 
-//go:embed tpl/*
+//go:embed template/*
 var f embed.FS
 
 // IndexHtml 单主机页面
 func IndexHtml() string {
-	data, _ := f.ReadFile("tpl/index.html")
+	data, _ := f.ReadFile("template/index.html")
 	return string(data)
 }
 
 // IndexFileHtml 多主机模式页面
 
 func IndexFileHtml() string {
-	data, _ := f.ReadFile("tpl/indexFile.html")
+	data, _ := f.ReadFile("template/indexFile.html")
 	return string(data)
 }
 
 func ErrorHtml() string {
-	data, _ := f.ReadFile("tpl/error.html")
+	data, _ := f.ReadFile("template/error.html")
 	return string(data)
 }
 
 // GolinHomeHtml 首页
 func GolinHomeHtml() string {
-	data, _ := f.ReadFile("tpl/golinHome.html")
+	data, _ := f.ReadFile("template/golinHome.html")
 	return string(data)
 }
 
 func GolinHistoryIndexHtml() string {
-	data, _ := f.ReadFile("tpl/golinHistoryIndex.html")
+	data, _ := f.ReadFile("template/golinHistoryIndex.html")
 	return string(data)
 }
 
 // DjHtml 模拟定级
 func DjHtml() string {
-	data, _ := f.ReadFile("tpl/dj.html")
+	data, _ := f.ReadFile("template/dj.html")
 	return string(data)
 }
 
 // DjLevelHtml 输出定级结果
 func DjLevelHtml() string {
-	data, _ := f.ReadFile("tpl/djLevel.html")
+	data, _ := f.ReadFile("template/djLevel.html")
 	return string(data)
 }
