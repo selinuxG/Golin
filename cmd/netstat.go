0894967c95d1ee41b0a4fc98344ddbd384a37c8c
via <overfor@icloud.com>
2024-08-30 14:37:01 +0800

    update netstat cmd short and long desc

--- cmd/netstat.go
+++ cmd/netstat.go
@@ -8,8 +8,8 @@ import (
 // netstatCmd represents the netstat command
 var netstatCmd = &cobra.Command{
 	Use:   "netstat",
-	Short: "获取CPU进程信息",
-	Long:  `降序获取CPU进程信息,输出:PID、PATH、`,
+	Short: "获取网络连接信息",
+	Long:  `降序获取网络连接信息 比如：pid、local_address、remote_address、status、program_name`,
 	Run:   clientinfo.Netstat,
 }
 
